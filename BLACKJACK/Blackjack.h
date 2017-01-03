cff5d63fb1a36a4d754c435450f540c7f8c6cd9f
martflorin <mart.florin@yahoo.com>
2017-01-03 12:40:45 +0200

    CreareCarte

--- BLACKJACK/Blackjack.h
+++ BLACKJACK/Blackjack.h
@@ -1,13 +1,15 @@
 #include<iostream>
 #include<stdlib.h>
-#define MAX_CARD 53
+#define MAX_CARTE 53
 using namespace std;
-unsigned int Deck[MAX_CARD];
-struct Card
+unsigned int Pachet[MAX_CARTE];
+struct Carte
 {
-     unsigned int number ;
-     char symbol[10];
+     unsigned int numar ;
+     char simbol;
 };
-bool CheckCard(unsigned int Card);
-unsigned int RandomCard();
-unsigned int VerificareSimbol(unsigned int Card);
+Carte CARTI[100];
+bool VerificareCarte(unsigned int Carte);
+unsigned int CarteAleatorie();
+unsigned int VerificareSimbol(unsigned int Carte);
+void CreareCarte();
--- BLACKJACK/main.cpp
+++ BLACKJACK/main.cpp
@@ -1,28 +1,51 @@
 #include "Blackjack.h"
-bool CheckCard(unsigned int Card)
+bool VerificareCarte(unsigned int Carte)
 {
-    if (Deck[Card]==1)
+    if (Pachet[Carte]==1)
         return 0;
     else
     {
-        Deck[Card]++;
+        Pachet[Carte]++;
         return 1;
     }
 }
-unsigned int RandomCard()
+unsigned int CarteAleatorie()
 {
-    unsigned int Card ;
-     Card=rand()%52+1;
-   while(CheckCard(Card)==0)
-    Card=rand()%52+1;
-    return Card;
+    unsigned int Carte ;
+     Carte=rand()%52+1;
+   while(VerificareCarte(Carte)==0)
+    Carte=rand()%52+1;
+    return Carte;
 }
-unsigned int VerificareSimbol(unsigned int Card)
+unsigned int VerificareSimbol(unsigned int Carte)
 {
-    if (Card<14) return 1;
-    else if (Card>13&&Card<27) return 2;
-    else if (Card>26&&Card<40) return 3;
-    else if (Card>39&&Card<53) return 4;
+    if (Carte<14) return 1;
+    else if (Carte>13&&Carte<27) return 2;
+    else if (Carte>26&&Carte<40) return 3;
+    else if (Carte>39&&Carte<53) return 4;
+}
+void CreareCarte()
+{
+    unsigned int i;
+    unsigned int Numar;
+    for (i=0;i<52;i++)
+    {
+        Numar=CarteAleatorie();
+        if (VerificareSimbol(Numar)==1)
+            CARTI[i].simbol='F';
+        else
+            if (VerificareSimbol(Numar)==2)
+                CARTI[i].simbol='R';
+            else
+                if (VerificareSimbol(Numar)==3)
+                    CARTI[i].simbol='I';
+                else
+                    CARTI[i].simbol='T';
+        if (Numar%13==0)
+            CARTI[i].numar=13;
+        else
+            CARTI[i].numar=Numar%13;
+    }
 }
 int main ()
 {
